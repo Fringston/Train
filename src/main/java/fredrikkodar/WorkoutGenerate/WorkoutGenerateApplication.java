9d497e9a5e2c2a0f6178b8178679b377f7db7407
Fredrik <91frerin@gafe.molndal.se>
2023-09-29 10:46:38 +0200

    initial commit

---
+++ src/main/java/fredrikkodar/WorkoutGenerate/WorkoutGenerateApplication.java
@@ -0,0 +1,13 @@
+package fredrikkodar.WorkoutGenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutGenerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutGenerateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fredrikkodar/WorkoutGenerate/WorkoutGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package fredrikkodar.WorkoutGenerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutGenerateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
